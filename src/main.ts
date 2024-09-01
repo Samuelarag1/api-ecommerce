c6c20dabbe939d751371f19f153b06e99a3e63a5
Samuel Aragon <samuelaragon616@gmail.com>
2024-09-01 00:08:35 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api Ecommerce')
+    .setDescription(
+      'this is a little description about the api rest for a ecommerce',
+    )
+    .setVersion('1.0')
+    .addTag('Ecommerce')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3001);
 }
 bootstrap();
